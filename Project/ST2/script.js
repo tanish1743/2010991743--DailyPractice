9a9a9d101b64ab59b431a340d48629a24a990280
TANISH JINDAL <78195040+tanish1743@users.noreply.github.com>
2022-08-09 12:16:16 +0530

    Project

--- Project/ST2/script.js
+++ Project/ST2/script.js
@@ -11,8 +11,8 @@ function myFunction() {
       t = Math.floor(t / 10);
     }
     borderRadius = Math.floor(sum / 5);
-    document.getElementById("create").style.width = sum + "px";
-    document.getElementById("create").style.height = sum + "px";
+    document.getElementById("create").style.width = sum*5 + "px";
+    document.getElementById("create").style.height = sum*5 + "px";
     document.getElementById("create").style.borderRadius = borderRadius + "px";
   } else {
     alert("Please enter a 5-digit number");
