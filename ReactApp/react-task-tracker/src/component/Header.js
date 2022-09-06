d1e3f2bdf81e0b4bc08378b80bb871c48653046a
TANISH JINDAL <78195040+tanish1743@users.noreply.github.com>
2022-09-06 11:04:37 +0530

    ReactApp

--- ReactApp/react-task-tracker/src/component/Header.js
+++ ReactApp/react-task-tracker/src/component/Header.js
@@ -9,14 +9,24 @@
 // export default Header;
 
 
-
+import "./Header.css";
 const Header=({title})=>{  
     return(
         <header>
-            <h1>Hello {title}</h1>  
+            <h1 style={headingStyle}>Hello {title}</h1> 
+            <h2 className="h2Head">H2 Heading</h2>
+            <h3 id="h3Head">H3 Heading</h3> 
+            <button className="btn">Add</button>
         </header>
     )
 };
+
+const headingStyle ={
+    color:"darkcyan",
+    backgroundColor:"lightblue",
+    textAlign:"center",
+};
+
 Header.defaultProps={
     title: "Task Tracker",
 };
