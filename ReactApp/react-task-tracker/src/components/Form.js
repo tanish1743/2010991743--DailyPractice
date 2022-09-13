c0896798916cb5c2fa2f9cbc2470e16759246f34
TANISH JINDAL <78195040+tanish1743@users.noreply.github.com>
2022-09-13 13:00:37 +0530

    ReactJS

---
+++ ReactApp/react-task-tracker/src/components/Form.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import './Form.css';
+
+class Form extends Component{
+    render(){
+        return(
+            <div>
+                <h1>Form Component</h1>
+            </div>
+        );
+    }
+}
+
+export default Form;
